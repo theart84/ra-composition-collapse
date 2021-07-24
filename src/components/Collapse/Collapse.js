8beb784c6c4378c19230328c234ebe4b2641616c
theart84 <artshadow74@gmail.com>
2021-07-24 20:10:51 +0500

    collapse alternative version

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Collapse = ({ isExpanded, collapsedLabel, expandedLabel, onExpandedChange, children }) => {
-  const [height, setHeight] = useState(0);
-  let childrenElement = null;
-
+const Collapse = ({
+  isExpanded,
+  collapsedLabel,
+  expandedLabel,
+  onExpandedChange,
+  height,
+  children,
+}) => {
   const onClickHandler = () => {
     onExpandedChange();
-      const { offsetHeight } = childrenElement.firstElementChild;
-      height === offsetHeight ? setHeight(0) : setHeight(offsetHeight);
   };
 
   const expandedElement = (
-    <span onClick={onClickHandler} className='title title__expanded'>
+    <span onClick={onClickHandler} className="title title__expanded">
       {expandedLabel}
-      <span className='icon icon-arrow-top' />
+      <span className="icon icon-arrow-top" />
     </span>
   );
   const collapsedElement = (
-    <span onClick={onClickHandler} className='title title__collapsed'>
+    <span onClick={onClickHandler} className="title title__collapsed">
       {collapsedLabel}
-      <span className='icon icon-arrow-down' />
+      <span className="icon icon-arrow-down" />
     </span>
   );
 
-  let collapseContentClasses = 'collapse-content';
+  let collapseContentClasses = "collapse-content";
 
   if (isExpanded) {
-    collapseContentClasses += ' collapse-content__visible';
+    collapseContentClasses += " collapse-content__visible";
   }
 
   return (
-    <div className='collapse'>
-      <div className='collapse-body'>
-        <div className={collapseContentClasses}
-             style={{ height: height }}
-             ref={(node) => (childrenElement = node)}
+    <div className="collapse">
+      <div className="collapse-body">
+        <div
+          className={collapseContentClasses}
+          style={{ height: isExpanded ? height : 0 }}
         >
           {children}
         </div>
@@ -51,6 +53,7 @@ Collapse.propTypes = {
   collapsedLabel: PropTypes.string.isRequired,
   expandedLabel: PropTypes.string.isRequired,
   onExpandedChange: PropTypes.func.isRequired,
+  height: PropTypes.number.isRequired
 };
 
 export default Collapse;
--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
 
-const Paragraph = ({ children }) => <p className="paragraph">{children}</p>;
+const Paragraph = ({ children, getHeight }) => {
+  const ref = useRef();
+  if (ref.current) {
+    const { offsetHeight } = ref.current;
+    getHeight(offsetHeight);
+  } 
+  
+  return (
+    <p ref={ref} className="paragraph">
+      {children}
+    </p>
+  );
+};
 
 Paragraph.propTypes = {
   children: PropTypes.node.isRequired,
+  getHeight: PropTypes.func.isRequired,
 };
 
 export default Paragraph;
